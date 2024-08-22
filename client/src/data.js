4163b7b0da65be4fa1cd9731e8716aba561f5426
Chamodh Jayasumana <chamodhinduwarajayasumana@gmail.com>
2024-08-22 13:33:35 +0530

    change and add some article dayta

--- client/src/data.js
+++ client/src/data.js
@@ -9,40 +9,40 @@ export const DUMMY_POSTS = [
         id: '1',
         thumbnail: Thumbnail1,
         category: 'education',
-        title: 'this is the title of very first post on this blog',
-        desc: 'werfwfddddddddddddddddddddddddddddddd',
+        title: 'Sławosz Uznański training at the European Astronaut Centre',
+        desc: 'Project astronaut Sławosz Uznański began his training at the European Astronaut Centre (EAC) in Cologne on 1 September 2023. Over the past months, he has been diligently preparing for his future space mission. Here’s a sneak peek into what these preparations looked like.  Back to the classroom Astronaut training includes many hours of theoretical classes ',
         authorID: 3
     },
     {
         id: '2',
         thumbnail: Thumbnail2,
         category: 'education',
-        title: 'this is the title of very first post on this blog',
-        desc: 'werfwfddddddddddddddddddddddddddddddd',
+        title: 'The Learning Spy',
+        desc: 'David Didau is a freelance writer, speaker and trainer. He runs the English Studies modules as part of BPP University’s PGCE course. From his blog, you can learn practical strategies like how to help students reach their potential or how to focus on cognitive science and evidence-based practice.',
         authorID: 1
     },
     {
         id: '3',
         thumbnail: Thumbnail3,
         category: 'education',
-        title: 'this is the title of very first post on this blog',
-        desc: 'werfwfddddddddddddddddddddddddddddddd',
+        title: 'United Kingdom Is Ruling The Apparel And Fashion World. Here’s How',
+        desc: 'Today, the UK textile industry holds 6 percent market share only in the fashion sector. The domestic market value of the UK fashion industry in Approximate 66 Million Pounds. Total household clothing consumption in the UK is above 6 Million Dollars per year. The country has advance spinning and weaving technologies to produce the high-quality fabric',
         authorID: 2
     },
     {
         id: '4',
         thumbnail: Thumbnail4,
         category: 'education',
-        title: 'this is the title of very first post on this blog',
-        desc: 'werfwfddddddddddddddddddddddddddddddd',
+        title: 'I need more space',
+        desc: 'The design of buildings and spaces can greatly affect how people feel and perform. Windows, wall colours and even plants in a room can improve your productivity and mood. In extreme environments, the layout of a habitat becomes even more important.',
         authorID: 13
     },
     {
         id: '5',
         thumbnail: Thumbnail5,
         category: 'education',
-        title: 'this is the title of very first post on this blog',
-        desc: 'werfwfddddddddddddddddddddddddddddddd',
+        title: 'The story behind the Muninn mission patch',
+        desc: 'Patches are an integral part of every astronaut mission. Space agencies print out a cloth reproduction that astronauts wear during their flight, a tradition that started sixty years ago',
         authorID: 11
     }
 ]
